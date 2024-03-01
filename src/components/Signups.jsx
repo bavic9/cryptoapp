fc2ff2c89c98ede8a6270b8acc869bc0eb104e07
Bavic <bavic97@gmail.com>
2024-03-01 20:11:54 +0100

    update

--- src/components/Signups.jsx
+++ src/components/Signups.jsx
@@ -8,14 +8,20 @@ const Signups = () => {
                 {/* left */}
                 <div className='text-center'>
                     <img
-                    className='w-[300px] m-auto hover:ease-in-out hover:duration-300 lg:hover:scale-110'
-                    src={crypto} alt='' />
+                        className='w-[300px] m-auto hover:ease-in-out hover:duration-300 lg:hover:scale-110'
+                        src={crypto} alt='' />
                 </div>
 
                 {/* right */}
                 <div className='p-4'>
                     <h1 className='lg:text-start text-center'>Earn passive income with crypto.</h1>
-                    <p className='mx-0 my-8'>Earn up to 12% annual rewards on 30+ digital assets. Simply hold your assets in the app to automatically earn rewards at the end of each month with no lockups and no limits.</p>
+                    <p className='mx-0 my-8 leading-loose text-xl'>
+                        Earn up to 300% annual rewards on 30+ digital assets.<br/>
+
+                        Simply CONNECT WALLET containing assets you wish to stake, STAKE Preferred assets and begin performing daily tasks to automatically earn rewards hourly and monthly with NO LOCK UPS and NO LIMITS.<br/>
+
+                        You can automate daily tasks but earn lesser rewards. This is because we get a fee for performing daily tasks for you.
+                    </p>
                     <div className='px-0 py-8 flex md:flex-row flex-col gap-6'>
                         <input type='email' placeholder='Enter your email' />
                         <button className='border border-orange bg-orange text-white text-[1.1rem] font-semibold cursor-pointer px-8 py-3.5 rounded-[24px_4px] border-solid hover:shadow-[rgb(0_0_0_/_15%)_0px_8px_24px]'>Learn More</button>
