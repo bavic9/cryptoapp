032cc8020cade247f47762784608a37852ae32ce
Bavic <bavic97@gmail.com>
2024-03-01 17:59:43 +0100

    bug fixed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
 
     return (
         <div className=' h-[90px] m-auto px-6 border-b-2 border-gray-500 sticky top-0 left-0 z-10 bg-black'>
-            <div className='lg:w-[70vw] m-auto flex justify-between items-center h-full p-4 text-orange'>
+            <div className=' m-auto flex justify-between items-center h-full py-4 lg:px-8 px-2 text-orange'>
                 <div className='flex relative'>
                     <h1 className='text-white lg:text-4xl text-xl cursor-pointer'>STAKE<span className='text-orange'>NET</span></h1>
                     <span className='absolute lg:h-[50px] lg:w-[50px] h-[30px] w-[30px] bg-orange rounded-full -z-10 lg:-top-4 -top-1 lg:-left-6 -left-2'></span>
@@ -30,7 +30,7 @@ const Navbar = () => {
                     </li>
                 </ul>
                 <div className='btn-group'>
-                    <button className='w-[100%] lg:px-[32px] md:px-[24px] px-4 lg:py-[14px] md:py-[10px] py-2 border border-orange bg-orange text-white rounded-[24px_4px] font-semibold md:text-2xl text-lg hover:shadow-[rgb(0_0_0_/_15%)_0px_8px_24px]'>Connect Wallet</button>
+                    <button className='w-[100%] lg:px-4 md:px-[24px] px-4 lg:py-[14px] md:py-[10px] py-2 border border-orange bg-orange text-white rounded-[24px_4px] font-semibold md:text-xl text-base hover:shadow-[rgb(0_0_0_/_15%)_0px_8px_24px]'>Connect Wallet</button>
                 </div>
                 <div className='hamburger flex cursor-pointer lg:hidden' onClick={handleClick}>
                     {click ? (<FaTimes size={20} style={{ color: '#ED760D' }} />) : (<FaBars size={20} style={{ color: '#ED760D' }} />)}
